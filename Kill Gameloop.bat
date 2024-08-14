04fbe2d30290112dbc0f8524a18468d497c3bf97
3MO SHNK <59408757+sh9k@users.noreply.github.com>
2024-08-14 18:26:16 +0300

    Update Kill Gameloop.bat

--- Kill Gameloop.bat
+++ Kill Gameloop.bat
@@ -1,6 +1,6 @@
 @echo off
 color 0b
-title "ANUBIS | anubischeats.com"
+title "3MO SHNK | Telegram :Kstoe6"
 net stop aow_drv
 net stop Tensafe
 taskkill /f /im appmarket.exe
@@ -13,6 +13,7 @@ taskkill /f /im GameLoader.exe
 taskkill /f /im TSettingCenter.exe
 taskkill /f /im syzs_dl_svr.exe
 taskkill /f /im AndroidEmulatorEn.exe
+taskkill /f /im AndroidEmulatorEx.exe
 net stop aow_drv
 net stop Tensafe
 
