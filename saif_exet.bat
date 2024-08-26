cde550c02efaad12ff4ee413c6ee7f4858d69d00
3MO SHNK <59408757+sh9k@users.noreply.github.com>
2024-08-26 14:31:46 +0300

    Add files via upload

---
+++ saif_exet.bat
@@ -0,0 +1,107 @@
+@echo off
+
+TaskKill /F /IM appmarket.exe
+
+TaskKill /F /IM androidemulator.exe
+
+TaskKill /F /IM aow_exe.exe
+
+TaskKill /F /IM QMEmulatorService.exe
+
+TaskKill /F /IM RuntimeBroker.exe
+
+taskkill /f /im adb.exe
+
+taskkill /f /im GameLoader.exe
+
+taskkill /f /im TSettingCenter.exe
+
+taskkill /f /im syzs_dl_svr.exe
+
+net stop aow_drv
+
+net stop Tensafe
+
+
+
+
+
+
+
+
+
+
+color b
+/s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
+cls 
+FOR /F "tokens=1, 2 * " %%V IN ('bcdedit') DO SET adminTest=%%V
+IF (%adminTest%)==(Access) goto noAdmin
+for /F " tokens=*" %%G in ('wevtutil.exe el') DO (call :do_clear "%%G")
+echo.
+echo Event Logs have been cleared! ^<press any key^>
+goto theEnd
+:do_clear
+echo clearing %1
+wevtutil.exe cl %1
+goto :eof
+:noAdmin
+echo You must run this script as an Administrator !
+echo ^<press any key^>
+cls
+pause
+
+
+
+
+
+
+
+
+@echo off
+del /s /f /q c:\windows\temp\*.*
+rd /s /q c:\windows\temp
+md c:\windows\temp
+del /s /f /q C:\WINDOWS\Prefetch
+del /s /f /q %temp%\*.*
+rd /s /q %temp%
+md %temp%
+deltree /y c:\windows\tempor~1
+deltree /y c:\windows\temp
+deltree /y c:\windows\tmp
+deltree /y c:\windows\ff*.tmp
+deltree /y c:\windows\history
+deltree /y c:\windows\cookies
+deltree /y c:\windows\recent
+deltree /y c:\windows\spool\printers
+del c:\WIN386.SWP
+cls 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
